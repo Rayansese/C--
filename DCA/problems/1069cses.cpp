1311ba0984bfa8f19b92c06a70d34ce42ca717ca
rayansese <thefastestmaninthelife@gmail.com>
2025-02-09 16:32:50 +0300

    modified:   DCA/problems/1069cses.cpp 	modified:   DCA/problems/1069cses.exe

--- DCA/problems/1069cses.cpp
+++ DCA/problems/1069cses.cpp
@@ -37,39 +37,56 @@
 cin >> str;
 	for(int i=0;i<str.size();i++)
  	{
- 		if
- (str[i]=='A'&&(str[i+1]=='A'||i==str.size()-1||str[i-1]=='A'))
- {
-	a++;
-	c=0;
-	g=0;
-	t=0;
- }
-
- 		else if (str[i]=='C'&&(str[i+1]=='C'||i==str.size()-1||str[i-1]=='C'))
-		{
-			c++;		
-			a=0;
-			g=0;
-			t=0;
-		}
-
-		else if 
- (str[i]=='G'&&(str[i+1]=='G'||i==str.size()-1||str[i-1]=='G'))
- {
-			g++;
-			a=0;
-			c=0;
-			t=0;
- }
-
-else if (str[i]=='T'&&(str[i+1]=='T'||i==str.size()-1||str[i-1]=='T')){
+if
+ (str[i]=='T'&&(str[i+1]=='T'||i==str.size()-1||str[i-1]=='T')){
 	t++;
+	if (t>c&&t>g&&t>a)
+	{
 	c=0;
 	g=0;
 	a=0;
+	}
  }
-     }
+}
+
+else if
+ (str[i]=='A'&&(str[i+1]=='A'||i==str.size()-1||str[i-1]=='A')){
+	a++;
+	if (a>c&&a>g&&a>t)
+	{
+	c=0;
+	t=0;
+	g=0;
+	}
+ }
+}
+
+else if
+ (str[i]=='C'&&(str[i+1]=='C'||i==str.size()-1||str[i-1]=='C')){
+	c++;
+	if (c>t&&c>g&&c>a)
+	{
+	g=0;
+	t=0;
+	a=0;
+	}
+ }
+
+
+else if
+ (str[i]=='G'&&(str[i+1]=='G'||i==str.size()-1||str[i-1]=='G')){
+	g++;
+	if (g>c&&g>t&&g>a)
+	{
+	c=0;
+	t=0;
+	a=0;
+	}
+ }
+
      
      cout << max(max(a,c),max(g,t));
- }
+ 
+
+
+
