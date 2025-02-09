0f71dd06795a6ff865f8a2e2550298519068ed8e
rayansese <thefastestmaninthelife@gmail.com>
2025-02-09 15:33:35 +0300

    Automated commit from mobile

---
+++ DCA/problems/1069cses.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+int main ()
+{
+	int a=0,c=0,g=0,t=0;
+	string str;
+	cin >> str;
+	for(int i=0;i<str.size();i++)
+	{
+		if
+(str[i]=='A'&&(str[i+1]=='A'||i==str.size()-1))
+		a++;
+		else if (str[i]=='C'&&(str[i+1]=='C'||i==str.size()-1))
+		c++;
+		else if 
+(str[i]=='G'&&(str[i+1]=='G'||i==str.size()-1))
+		g++;
+		else if (str[i]=='T'&&(str[i+1]=='T'||i==str.size()-1))
+		
+		
+t++;
+	}
+	
+	cout << max(max(a,c),max(g,t));
+}
