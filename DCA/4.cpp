df218e92e3d28b59b5a8f791f55e2de3391c97fa
rayansese <thefastestmaninthelife@gmail.com>
2025-02-27 20:44:01 +0300

    modified:   DCA/4.cpp 	new file:   DCA/4.exe 	new file:   "DCA/problems/Chewba\321\201ca and Number.cpp"

--- DCA/4.cpp
+++ DCA/4.cpp
@@ -1,22 +1,82 @@
-#include <iostream>
-string swap1 (string s)
-{
-    for (int i=0;i<s.size();i++)
-    {
-        for (int j=i;j<s.size();j++)
-        {
-            if (s[i]>s[j])
-                swap(s[i],s[j]);
-        }
-        
-    }
-    return s;
-}
-using namespace std;
+// #include <iostream>
+// using namespace std;
+// int main () {
 
+
+
+// string s="7";
+// int n=stoi(s)-5;
+
+
+
+
+
+
+
+
+
+// return 0;
+// }
+
+// #include <bits/stdc++.h>
+
+// using namespace std;
+// int main () {
+
+// string s ;cin >> s;
+// int n;
+// for (int i = 0; i <s.size(); i++) {
+// n = min(stoi(s[i]), stoi(abs(s[i] -9)))
+// }
+
+
+
+// cout << s;
+
+
+
+
+
+
+
+
+// return 0;
+// }
+
+#include <bits/stdc++.h>
+
+using namespace std;
 int main () {
-string s ="54321";
-cout << swap1(s);
+
+string s ;cin >> s;
+int a,b;
+for (int i = 0; i <s.size(); i++) {
+//s1[i]= to_string( min(stoi(to_string(s[i])) ,abs(stoi(to_string(s[i])) -9 )));
+//s= stoi(s[i]) -9 ;
+// a= stoi(to_string(s[i]));
+// b= abs(stoi(to_string(s[i])) -9);
+//  if (abs(b)<a) {
+//  s[i]= to_string(b)[0];
+//  }
+// }
+a = s[i] - '0';
+b = 9 - a;
+ if (b<a) {
+s[i] = b + '0';
+
+}
+}
+if (s[0]== '0' && s.size() > 1) {
+    s[0] = '9';
+}
+
+cout << s;
+
+
+
+
+
+
 
 
 return 0;
---
+++ DCA/problems/Chewbaсca and Number.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int main () {
+
+string s,s1 ;cin >> s;
+for (int i = 0; i <s.size(); i++) {
+//s1[i]= to_string( min(stoi(s[i]) , abs(stoi(s[i]) -9 )));
+//s= stoi(s[i]) -9 ;
+
+}
+
+
+
+cout << s;
+
+
+
+
+
+
+
+
+return 0;
+}
+
