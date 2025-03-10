b3862fbc88a41da5143b094dc40b4391c5a8806e
rayansese <thefastestmaninthelife@gmail.com>
2025-03-10 18:37:02 +0300

    modified:   DCA/3.cpp 	modified:   DCA/3.exe 	modified:   DCA/4.cpp 	modified:   DCA/4.exe 	new file:   DCA/problems/Final Verdict.cpp

--- DCA/3.cpp
+++ DCA/3.cpp
@@ -2,29 +2,57 @@
 
 using namespace std;
 int main () {
+
 int n;cin >> n;
-string s,s1;
 for (int i=0;i<n;i++)
 {
-
-cin >> s;
- char arr [s.size()-1];
-for (int j=0;j<s.size()-2;j++)
+    int a,x;
+    cin >> a >> x;
+    vector<int>vec,v;
+    for (int j=0;j<a;j++)
+    {
+        cin >> vec[j];
+    }
+    if (n%2==0)
+    {
+        while (n>2)
+        {
+for (int k=0;k<n/2;k++)
 {
-//s1[j]=s[j];
-arr[j]=s[j];
+
+v[0]+=vec[k];
+
+
 
 }
-for (int k=0;k<s.size()-2;k++)
+v[0]=v[0]/(n/2);
+for (int k=n/2+1;k<n;k++)
 {
-cout << arr[k];
 
-}
-cout << 'i'<< endl ;
+v[1]+=vec[k];
 
 
 
 }
+
+v[1]=v[1]/(n/2);
+vec=v;
+n=2;
+    }
+}
+
+}
+
+
+
+
+
+
+
+
+
+
 
 return 0;
 }
+
--- DCA/4.cpp
+++ DCA/4.cpp
@@ -1,84 +1,13 @@
-// #include <iostream>
-// using namespace std;
-// int main () {
-
-
-
-// string s="7";
-// int n=stoi(s)-5;
-
-
-
-
-
-
-
-
-
-// return 0;
-// }
-
-// #include <bits/stdc++.h>
-
-// using namespace std;
-// int main () {
-
-// string s ;cin >> s;
-// int n;
-// for (int i = 0; i <s.size(); i++) {
-// n = min(stoi(s[i]), stoi(abs(s[i] -9)))
-// }
-
-
-
-// cout << s;
-
-
-
-
-
-
-
-
-// return 0;
-// }
-
 #include <bits/stdc++.h>
 
 using namespace std;
 int main () {
-
-string s ;cin >> s;
-int a,b;
-for (int i = 0; i <s.size(); i++) {
-//s1[i]= to_string( min(stoi(to_string(s[i])) ,abs(stoi(to_string(s[i])) -9 )));
-//s= stoi(s[i]) -9 ;
-// a= stoi(to_string(s[i]));
-// b= abs(stoi(to_string(s[i])) -9);
-//  if (abs(b)<a) {
-//  s[i]= to_string(b)[0];
-//  }
-// }
-a = s[i] - '0';
-b = 9 - a;
- if (b<a) {
-s[i] = b + '0';
-
+vector<int>vec = {1,2,3,4};
+vector<int>v = vec;
+for (int i=0;i<4;i++)
+{
+cout << v[i] << endl;
 }
-}
-if (s[0]== '0' && s.size() > 1) {
-    s[0] = '9';
-}
-
-cout << s;
-
-
-
-
-
-
-
-
 return 0;
 }
 
