20a3d680578dd5c1226a36121b257ccef4263e83
rayansese <thefastestmaninthelife@gmail.com>
2025-04-16 14:06:45 +0300

    modified:   DCA/4.cpp 	modified:   DCA/4.exe 	new file:   DCA/problems/Move Bracketsv1.exe

--- DCA/4.cpp
+++ DCA/4.cpp
@@ -1,13 +1,59 @@
-#include <bits/stdc++.h>
-
+#include <iostream>
+#include <deque>
 using namespace std;
-int main () {
-vector<int>vec = {1,2,3,4};
-vector<int>v = vec;
-for (int i=0;i<4;i++)
+int main ()
 {
-cout << v[i] << endl;
-}
-return 0;
-}
+	int n;cin >> n;
+	int y=0;
+	for (int i=0;i<n;i++)
+	{
+		int t;cin >> t; 
+		deque<char> q (t) ;
+		//for (int j=0;j<t;j++)
+//		{
+//			
+//			char c;
+//			cin >> c;
+//			q.push_back(c);
+//		}
 
+for (auto it = q.begin(); it != q.end();it++ ){
+			char c;cin >> c;
+			q.push_back(c);
+	
+		}
+
+
+    for (auto it = q.begin(); it != q.end();it++ ){
+        if (*it== ')' && *it-1=='(')
+			{
+			q.erase(it);
+			q.erase(it -1);
+			}
+            
+    }
+		auto d = q.begin() ;
+		int s=q.size()/2;
+for (auto j=q.begin();j!=q.begin()+(q.size()/2);j++,d++)
+		{
+			if (*j ==')')
+			{
+				q.erase(j);
+				q.push_back(')');
+				y++;
+			}	
+			
+		}
+		for (;d!= q.end();++d)
+		{
+			if (*d == '(')
+			{
+				q.erase(d);
+				q.push_front(')');
+				y++;
+			}
+		}
+		cout << y << endl;
+	}
+	
+}
